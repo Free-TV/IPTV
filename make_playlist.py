bf1d93323d6c0ef03d30623ef349619ea0b3c5f4
Kálmán „KAMI” Szalai <kami911@gmail.com>
2022-07-23 09:26:45 +0200

    Add initial support of EPG id

--- make_playlist.py
+++ make_playlist.py
@@ -8,16 +8,24 @@ class Channel():
     def __init__(self, group, md_line):
         self.group = group
         md_line = md_line.strip()
-        (before, number, name, url, logo, after) = md_line.split("|")
+        try:
+            (before, number, name, url, logo, epg, after) = md_line.split("|")
+        except ValueError:
+            (before, number, name, url, logo, after) = md_line.split("|")
+            epg = None
         self.number = number.strip()
         self.name = name.strip()
         self.url = url.strip()
         self.url = self.url[self.url.find("(")+1:self.url.rfind(")")]
         self.logo = logo.strip()
         self.logo = self.logo[self.logo.find('src="')+5:self.logo.rfind('"')]
+        self.epg = epg.strip() if epg else None
 
     def to_m3u_line(self):
-        return (f'#EXTINF:-1 tvg-name="{self.name}" tvg-logo="{self.logo}" group-title="{self.group}",{self.name}\n{self.url}')
+        if self.epg is None:
+            return (f'#EXTINF:-1 tvg-name="{self.name}" tvg-logo="{self.logo}" group-title="{self.group}",{self.name}\n{self.url}')
+        else:
+            return (f'#EXTINF:-1 tvg-name="{self.name}" tvg-logo="{self.logo}" tvg-id="{self.epg}" group-title="{self.group}",{self.name}\n{self.url}')
 
 if __name__ == "__main__":
     with open("playlist.m3u8", "w", encoding='utf-8') as playlist:
@@ -35,4 +43,3 @@ if __name__ == "__main__":
                         continue
                     channel = Channel(group, line)
                     print(channel.to_m3u_line(), file=playlist)
-                    
